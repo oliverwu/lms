a7d70627313b89055aed9d0735dd91b188d2d952
oliver <oliver.wu.1987@gmail.com>
2018-11-18 17:18:49 +1100

    refactor the index.js in src to use redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Component/App/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './Reducers/index';
+import middleware from './Middlewares/index';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
